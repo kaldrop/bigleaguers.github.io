5b319408d17f8a369918aad3e8ab93a764ca3f90
kaldrop <67424040+kaldrop@users.noreply.github.com>
2021-02-02 20:22:08 +0100

    mi_portfolio

--- js/slider.js
+++
@@ -1,9 +0,0 @@
-const slide = document.getElementById('glide')
-
-new Glide(slide, {
-    type: 'carousel',
-    startAt: 0,
-    preView: 1,
-    animationDuration: 800,
-    animationTimingFunc: 'linear',
-}).mount();
